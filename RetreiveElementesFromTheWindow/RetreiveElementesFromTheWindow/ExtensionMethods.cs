7047623ff8929d5b4e101b48782c14b87a8a5a55
Vaibhav Srivastava <vaibhavsrivastava95@gmail.com>
2019-01-18 13:48:05 +0530

    Implemented Logical Tree for Getting Child Elements From Window , User , Pages

---
+++ RetreiveElementesFromTheWindow/RetreiveElementesFromTheWindow/ExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RetreiveElementesFromTheWindow
+{
+	public static class ExtensionMethods
+	{
+		public static IEnumerable<T> FindLogicalChildren<T>(DependencyObject depObj) where T : DependencyObject
+		{
+			if (depObj != null)
+			{
+				foreach (object rawChild in LogicalTreeHelper.GetChildren(depObj))
+				{
+					if (rawChild is DependencyObject)
+					{
+						DependencyObject child = (DependencyObject)rawChild;
+						if (child is T)
+						{
+							yield return (T)child;
+						}
+
+						foreach (T childOfChild in FindLogicalChildren<T>(child))
+						{
+							yield return childOfChild;
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ RetreiveElementesFromTheWindow/RetreiveElementesFromTheWindow/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RetreiveElementesFromTheWindow
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			RetreiveElementFromTheWindow(this);
+		}
+		private void RetreiveElementFromTheWindow(DependencyObject thisObject)
+		{
+			// Get all available TextBoxes from object(Window, UserControl Or Pages)
+			foreach (TextBox txtBox in ExtensionMethods.FindLogicalChildren<TextBox>(thisObject))
+			{
+				
+			}
+			// Get all available TextBlocks from object(Window, UserControl Or Pages)
+			foreach (TextBlock txtBox in ExtensionMethods.FindLogicalChildren<TextBlock>(thisObject))
+			{
+
+			}
+		}
+	}
+}
