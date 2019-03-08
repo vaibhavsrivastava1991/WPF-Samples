5e88b0c7d4887f30d7214d0cc99d256beadef293
Vaibhav Srivastava <vaibhavsrivastava95@gmail.com>
2019-03-08 19:19:11 +0530

    Add Global Exception Termination Application Handler

---
+++ GlobalExceptionHandler/GlobalExceptionHandler/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GlobalExceptionHandler
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			//Get Current Domain In Constructor...
+			AppDomain currentDomain = AppDomain.CurrentDomain;
+			//Add UnhandlerExceptionHandler -> MyExceptionHandlerGlobally
+			currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyExceptionHandlerGlobally);
+		}
+		void MyExceptionHandlerGlobally(object sender, UnhandledExceptionEventArgs args)
+		{
+			Exception e = (Exception)args.ExceptionObject;
+			if (args.IsTerminating)
+			{
+				try
+				{
+					//Do Your Work On Terminating..
+
+
+				}
+				catch (Exception ex)
+				{
+					
+				}
+			}
+		}
+	}
+}
