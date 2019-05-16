df644771b12009af8513d210c7f3c6e520d2a88d
Vaibhav Srivastava <46020723+vaibhavsrivastava1991@users.noreply.github.com>
2019-05-16 19:18:12 +0530

    Add files via upload

---
+++ AnimatedGreen/AnimatedGreen/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AnimatedGreen
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public static Thread thread;
+		public static SplashWindow w;
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			try
+			{
+				//var splashScreen = new SplashWindow();
+				////this.MainWindow = splashScreen;
+				////this.MainWindow.Close();
+				//splashScreen.Show();
+				OnCreateNewWindow(null,null);
+			}
+			catch (Exception ex)
+			{
+
+			}
+
+		}
+		private void OnCreateNewWindow(object sender, RoutedEventArgs e)
+		{
+			thread = new Thread(() =>
+			{
+				w = new SplashWindow();
+				w.Show();
+
+				w.Closed += (sender2, e2) =>
+					w.Dispatcher.InvokeShutdown();
+
+				System.Windows.Threading.Dispatcher.Run();
+			});
+
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();
+		}
+	}
+}
---
+++ AnimatedGreen/AnimatedGreen/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AnimatedGreen
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{public delegate void ReadyToShowDelegate(object sender, EventArgs args);
+
+        public event ReadyToShowDelegate ReadyToShow;
+
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+			//Dispatcher.BeginInvoke(new Action(() =>
+			//{
+			    
+				//for (int i = 0; i < 1900; i++)
+				//{
+				//	i = 10;
+				//}
+			//}));
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            // This timer imitates a time-consuming operation (or a whole bunch of
+            // such operations).
+            // When done, it raises a custom event to let the splash screen know that its time is up.
+
+            timer.Stop();
+
+            if (ReadyToShow != null)
+            {
+                ReadyToShow(this, null);
+            }
+        }
+	}
+}
